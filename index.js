36a01ae731abb0181e7b1aa19558f6bf1ffe1def
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-08 23:57:34 +0545

    Js started

---
+++ index.js
@@ -0,0 +1,55 @@
+// Ways to print in JavaScript
+console.log("Hello world");
+//alert("me")
+document.write("Hello This is document write");
+
+
+// JavaScript console API
+console.log("Hello world", 2 + 8, "Errors");
+console.warn("THis is warning");
+console.error("This is an Error");
+// console.clear() 
+
+
+// JS variables
+
+// Variables stores data values 
+
+var num = 34;
+var num2 = 56;
+console.log(num+num2);
+
+
+// data types in js
+
+// num 
+var num1 = 455;
+var num2 = 20;
+
+//string
+var str1 = "This is a string";
+var str2 = "this is also a string";
+
+//objects
+var marks = {
+    ravi:20,
+    shyam: 89,
+    hari: 98
+}
+
+
+console.log(marks);
+
+
+// Booleans 
+var a = true;
+var b = false;
+
+/* 
+At the very high level, there are two types of data types in JS 
+1. Primitive : undefined, null, number, string, boolean, symbol
+2. Reference data types : Arrays and objects 
+
+*/
+
+var arr = [1,2,3,4,5]
