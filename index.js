92f03a6060bed1f049a9e5d52b6d17994400b663
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-10 23:49:15 +0545

    learning

--- index.js
+++ index.js
@@ -53,3 +53,87 @@ At the very high level, there are two types of data types in JS
 */
 
 var arr = [1,2,3,4,5]
+
+
+// Operators 
+//Arithmetic Operators 
+
+var a = 100;
+var b = 10;
+
+console.log("The value of a + b is", a+b);
+console.log("The value of a - b is", a-b);
+
+
+console.log("The value of a * b is", a*b);
+console.log("The value of a / b is", a/b);
+
+
+// logical Operators AND OR NOT
+
+// console.log(true && true)
+// console.log(true && false)
+
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// console.log(!true)
+
+
+// JS Functions 
+// DRY = DO not repeat yourself. 
+
+function avg(a, b){
+    return (a+b)/2;
+
+}
+
+c = avg(4, 6);
+console.log("average", c)
+
+
+// COnditions in JS 
+
+
+// if else statement
+var age = 4;
+if (age > 18){
+    console.log('You are not a kid')
+}
+
+else {
+    console.log('you are a kid')
+}
+
+
+
+// Loops 
+
+var arr = [1,2,3,4,5,6,7];
+console.log(arr);
+
+for(var i =0;i<arr.length;i++){
+    console.log(arr[i])
+}
+
+
+arr.forEach(function(element){
+    console.log(element);
+})
+
+
+// let j=0;
+// const ac = 0;
+// acc++
+
+
+
+let j = 0;
+while(j<arr.length){
+    console.log(arr[j]);
+    j++;
+    
+}
