6b597aa08f24e128218c69ec909cc04139b2b2e5
prabinshrestha737 <prabinshrestha737@gmail.com>
2022-07-14 22:53:59 +0545

    word guess added

---
+++ wordguess/script.js
@@ -0,0 +1,76 @@
+const inputs = document.querySelector(".inputs"),
+resetBtn = document.querySelector(".reset-btn"),
+hint = document.querySelector(".hint span"),
+typingInput = document.querySelector(".typing-input");
+wrongLetter = document.querySelector(".wrong-letter span");
+guessLeft = document.querySelector(".guess-left span");
+
+let word, corrects = [], incorrects = [], maxGuesses;
+
+function randomWord() {
+    //getting random objects from wordList
+    let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(ranObj);
+
+    maxGuesses = 8;
+    word = ranObj.word; //getting word of random object
+    console.log(word);
+
+    hint.innerText = ranObj.hint;
+    guessLeft.innerText = maxGuesses;
+
+    let html = "";
+
+    for (let i = 0; i< word.length;i++) {
+        html += `<input type="text" disabled>`;
+
+    }
+    inputs.innerHTML = html;
+
+
+}
+
+randomWord();
+
+function initGame(e){
+    let key = e.target.value;
+    if(key.match(/^[A-Za-z]+$/) && !incorrects.includes( `${key}`) && !corrects.includes(key)) {
+        console.log(key);
+        if(word.includes(key)){ // if user letter found in the word
+            for (let i = 0;  i <word.length;i++) {
+                if(word[i]=== key){
+                    corrects.push(key);
+                    inputs.querySelectorAll("input")[i].value= key;
+
+                }
+
+            }
+        } else {
+            
+            maxGuesses--; // Decrement maxGuesses by 1 
+            incorrects.push(`${key}`);
+        }
+        guessLeft.innerHTML = maxGuesses
+        wrongLetter.innerHTML = incorrects;
+    }
+    
+    if (maxGuesses<1) { // if user couldn't found all letters 
+        alert("Game over! You don't have remaining guesses");
+        for (let i = 0;  i <word.length;i++) {
+            if(word[i]=== key){
+                corrects.push(key);
+                inputs.querySelectorAll("input")[i].value= key;
+
+            }
+
+        }
+        
+
+    }
+    typingInput.value = "";
+   
+}
+
+resetBtn.addEventListener("click", randomWord);
+typingInput.addEventListener("input", initGame);
+document.addEventListener("keydown", () => typingInput.focus());
